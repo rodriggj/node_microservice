c5b6c0cd56684130055c4bbd9d9ac790fcec01a2
Gabriel Rodriguez <gabrielrodriguez@elements-mac0286.local>
2020-08-16 10:09:06 -0600

    Completed S2 L30 on Receiving events

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -8,9 +8,9 @@ app.use(bodyParser.json());
 
 app.post('/events', (req, res)=>{
     const event = req.body; 
-    axios.post('http://localhost:4000/events',event);   //posts service
-    axios.post('http://localhost:4001/events',event);   //comments service
-    axios.post('http://localhost:4002/events',event);   //query service (TBD)
+    axios.post('http://localhost:4000/events', event);   //posts service
+    axios.post('http://localhost:4001/events', event);   //comments service
+    //axios.post('http://localhost:4002/events',event);   //query service (TBD)
     res.send( {eventStatus: "OK"} );
 });
 
