341951edb7b6aced1ff452e2231719b971af4771
rodriggj <gabriel.rodriguez.ctr@gmail.com>
2021-03-21 11:24:47 -0600

    added redis components and all the associated lesson info

--- visits/front-end/index.js
+++ visits/front-end/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const redis = require('redis');
 const app = express();
+const client = redis.createClient();
 const PORT = 4000;
 
+client.set('visits', 0);
+
 app.get('/', (req, res) => {
-    res.send(`You have visited this app X number of times.`)
+    // client.get('visits', (err, visits) => {
+    //     res.send('Number of visits is ' + visits);
+    //     client.set('visits', parseInt(visits) + 1);
+    // })
+    res.send('This is the new docker bld with redis temp disabled')
 });
 
 app.listen(PORT, console.log(`Server is up and listening on port: ${PORT}`))
